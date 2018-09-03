3fe4fbdcee4133fdad9565965673d8dd3e822b5c
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-09-02 22:23:32 -0400

    change link on index.html

--- src/Components/Home/matches/index.js
+++ src/Components/Home/matches/index.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import { Tag } from "../../Ui/misc"
-import Blocks from "./Blocks";
+import { Tag } from '../../Ui/misc';
+import Blocks from './Blocks';
 
 const MatchesHome = () => {
-    return (
-        <div className="home_matches_wrapper">
-            <div className="container">
-                <Tag
-                    bck="#0e1731"
-                    size="50px"
-                    color="#ffff"
-                >
-                    Matches
-                </Tag>
+  return (
+    <div className="home_matches_wrapper">
+      <div className="container">
+        <Tag bck="#0e1731" size="50px" color="#ffff">
+          Matches
+        </Tag>
 
-                <Blocks/>
+        <Blocks />
 
-                <Tag
-                    bck="#ffffff"
-                    size="22px"
-                    color="#0e1731"
-                    link={true}
-                    linkto="/the_team"
-                >
-                    see more matches
-                </Tag>
-
-            </div>
-        </div>
-    );
+        <Tag
+          bck="#ffffff"
+          size="22px"
+          color="#0e1731"
+          link={true}
+          linkto="/the_matches"
+        >
+          see more matches
+        </Tag>
+      </div>
+    </div>
+  );
 };
 
 export default MatchesHome;
